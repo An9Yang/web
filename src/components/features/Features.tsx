87ba13a769e39a855fb4b95186b4afe1aa82c31e
AY <ay2494@nyu.edu>
2025-12-18 14:08:08 +0800

    vercel

--- src/components/features/Features.tsx
+++ src/components/features/Features.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { MessageSquare, Server, Globe2, Clock } from "lucide-react";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 
 const features = [
     {
@@ -31,7 +31,7 @@ const features = [
     },
 ];
 
-const containerVariants = {
+const containerVariants: Variants = {
     hidden: { opacity: 0 },
     show: {
         opacity: 1,
@@ -41,7 +41,7 @@ const containerVariants = {
     }
 };
 
-const itemVariants = {
+const itemVariants: Variants = {
     hidden: { opacity: 0, y: 30 },
     show: { opacity: 1, y: 0, transition: { type: "spring", stiffness: 50 } }
 };
